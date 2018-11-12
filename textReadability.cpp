68b65905a4f420226a770db799fd72c307654ee9
Chiebuka Lebechi <chiebuka.lebechi@gmail.com>
2018-11-12 13:13:02 -0900

    created textReadability.cpp

---
+++ textReadability.cpp
@@ -0,0 +1,13 @@
+// name : textReadability.cpp
+// author : Kline and Lebechi
+// purpose : A program to read a text file and compute the two Flesch-Kincaid readability scores for the text: reading ease and grade level.
+
+
+
+
+
+
+int main() {
+
+
+}
