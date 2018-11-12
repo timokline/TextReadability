5fbc577e880e1769864f182aa9a3cd9cf603d22f
timothyakline <timothyakline@users.noreply.github.com>
2018-11-12 13:16:25 -0900

    iostream library
    included iostream library

--- textReadability.cpp
+++ textReadability.cpp
@@ -2,7 +2,7 @@
 // author : Kline and Lebechi
 // purpose : A program to read a text file and compute the two Flesch-Kincaid readability scores for the text: reading ease and grade level.
 
-
+#include<iostream>
 
 
 
